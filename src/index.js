8791a10084f4d9ea7e3f58e145f1f29924b03ad5
yasakei <yas.studios.en@gmail.com>
2025-08-06 20:16:28 +0600

    depricated

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import './components/AccessStatusPage.css'; // This ensures the CSS is included.
-
-export { useAccessControl } from './hooks/useAccessControl';
-export { AccessControlGate } from './components/AccessControlGate';
